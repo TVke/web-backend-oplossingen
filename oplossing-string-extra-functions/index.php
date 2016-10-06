27c7986878b6f782d2b5a96a6a461d9cc995500e
Thomas Verhelst <Thomas@Pro-van-Thomas.local>
2016-10-06 21:40:38 +0200

    add of part 2 of extra string functions

--- oplossing-string-extra-functions/index.php
+++ oplossing-string-extra-functions/index.php
@@ -1,5 +1,7 @@
 <?php 
 	
+	// deel 1
+/*
 	$fruit = "kokosnoot";
 	$fruitlen = strlen($fruit);
 	$oInFruit = strpos($fruit,"o");
@@ -13,8 +15,22 @@
 	}else{
 		$oSte = "geen enkele";
 	}
+*/
 	
-	
+	//deel 2
+	$fruit = "ananas";
+	$aInFruit = strrpos($fruit,"a");
+	if($aInFruit){
+		$aInFruit++;
+		if($aInFruit > 1 && $aInFruit < 20){
+			$aSte = "de ".$aInFruit."de";
+		}else{
+			$aSte = "de ".$aInFruit."ste";
+		}
+	}else{
+		$aSte = "geen enkele";
+	}
+	$fruit = strtoupper($fruit);
 ?>
 
 <!DOCTYPE html>
@@ -25,10 +41,12 @@
 	<body>
 		<h1>Oplossing-string-extra-functions</h1>
 		<h2>Deel 1</h2>
+<!--
 		<p>Het woord <?= $fruit?> telt <?= $fruitlen?> karakters</p>
 		<p>De eerste 'o' in <?= $fruit?> bevindt zich op <?= $oSte?> plaats</p>
+-->
 		<h2>Deel 2</h2>
-		<p></p>
+		<p>De laatste 'a' in <?= $fruit?> bevindt zich op <?= $aSte?> plaats</p>
 		<h2>Deel 3</h2>
 		<p></p>
 	</body>
