7c817d23ce48fed4547b39ace993a047721af910
TVke <thomas.verhelst@student.kdg.be>
2016-10-12 20:43:39 +0200

    add full solution of array functions

--- oplossing-arrays-functions/index.php
+++ oplossing-arrays-functions/index.php
@@ -10,10 +10,16 @@
 	}
 	
 	// deel 2
-	
+	$dieren2=array("geit","schaap","krokodil","eend","baardhagedis");
+	arsort($dieren2);
+	$zoogdieren=array("varken","dolfijn","paard");
+	$dierenLijst=$dieren2+$zoogdieren;
 	
 	// deel 3
-	
+	$waarden=array(8, 7, 8, 7, 3, 2, 1, 2, 4);
+	$waardenUnique=array_unique($waarden);
+	$waardenSorted=$waardenUnique;
+	arsort($waardenSorted);
 	
 ?>
 
@@ -28,8 +34,12 @@
 		<p>Er zitten <?= $aantalDieren?> in deze array: <?php print_r($dieren)?>.</p>
 		<p>Het woord "<?= $teZoekenDier?>" is <?= $zoekResultaat?>gevonden in de bovenstaande array.</p>
 		<h2>deel 2</h2>
-		<p></p>
+		<p>geordend: <?php print_r($dieren2);?></p>
+		<p>Zoogdieren: <?php print_r($zoogdieren);?></p>
+		<p>dierenlijst: <?php print_r($dierenLijst);?></p>
 		<h2>deel 3</h2>
-		<p></p>
+		<p>De originele array: <?php print_r($waarden);?></p>
+		<p>De unieke waarden array: <?php print_r($waardenUnique);?></p>
+		<p>De gesoreerde array: <?php print_r($waardenSorted);?></p>
 	</body>
 </html>
