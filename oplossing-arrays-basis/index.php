e024fff50b4c403d6a1e6f26a085d91ddcdfc927
Thomas <Thomas@Pro-van-Thomas.local>
2016-10-12 19:53:47 +0200

    add part two of arrays basics

--- oplossing-arrays-basis/index.php
+++ oplossing-arrays-basis/index.php
@@ -20,6 +20,27 @@
 	
 	// deel 2
 	
+	$getallen=array(1, 2, 3, 4, 5);
+	
+	$multiplied=1;
+	for($i=0,$ilen=count($getallen);$i<$ilen;$i++){
+		$multiplied*=$getallen[$i];
+	}
+	
+	$uneven=array();
+	for($i=0,$ilen=count($getallen);$i<$ilen;$i++){
+		if($getallen[$i]%2==1){
+			array_push($uneven, $getallen[$i]);
+		}
+	}
+	
+	$reversed=array_reverse($getallen);
+	
+	$reversPlusNormal=array();
+	for($i=0,$ilen=count($getallen);$i<$ilen;$i++){
+		$reversPlusNormal[$i]=$getallen[$i]+$reversed[$i];
+	}
+	
 ?>
 
 <!DOCTYPE html>
@@ -30,8 +51,12 @@
 	<body>
 		<h1>arrays basis</h1>
 		<h2>deel 1</h2>
-		<p><?= var_dump($voertuigen);?></p>
+		<p><?php var_dump($voertuigen);?></p>
 		<h2>deel 2</h2>
-		<p></p>
+		<p>de array: <?php print_r($getallen);?></p>
+		<p>vermenigvuldigd: <?= $multiplied?></p>
+		<p>de oneven: <?php print_r($uneven);?></p>
+		<p>achterstevoren: <?php print_r($reversed);?></p>
+		<p>som van normaal en achterstevoren: <?php print_r($reversPlusNormal);?></p>
 	</body>
 </html>
