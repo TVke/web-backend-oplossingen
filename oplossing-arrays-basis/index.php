b07b37a5e4f032d28e5a9d2781d2e973c486ab2e
Thomas <Thomas@Pro-van-Thomas.local>
2016-10-12 19:28:23 +0200

    add part 1 of arrays basics solution

--- oplossing-arrays-basis/index.php
+++ oplossing-arrays-basis/index.php
@@ -1,7 +1,22 @@
 <?php
 	
 	// deel 1
+	$dieren=array("hond","kat","paard","vis","egel","eland","sprinkhaan","krekel","lama","emoe");
+	$dierenoptie2[0]="Hond";
+	$dierenoptie2[1]="Kat";
+	$dierenoptie2[2]="Paard";
+	$dierenoptie2[3]="Vis";
+	$dierenoptie2[4]="Egel";
+	$dierenoptie2[5]="Eland";
+	$dierenoptie2[6]="Sprinkhaan";
+	$dierenoptie2[7]="Krekel";
+	$dierenoptie2[8]="Lama";
+	$dierenoptie2[9]="Emoe";
 	
+	$voertuigen=array(
+	"landvoertuigen"=>array("rolschaatsen","fiets","skateboard"),
+	"luchtvoertuigen"=>"vliegtuig",
+	"watervoertuigen"=>"boot");
 	
 	// deel 2
 	
@@ -15,7 +30,7 @@
 	<body>
 		<h1>arrays basis</h1>
 		<h2>deel 1</h2>
-		<p></p>
+		<p><?= var_dump($voertuigen);?></p>
 		<h2>deel 2</h2>
 		<p></p>
 	</body>
