f21ee57a9cb1d3a759517862639bcd9ab24be5bf
TVke <thomas.verhelst@student.kdg.be>
2016-10-12 23:36:05 +0200

    add start of solution functions recursive

---
+++ oplossing-functions-recursive/index.php
@@ -0,0 +1,22 @@
+<?php
+	
+	// deel 1
+	
+	
+	// deel 2
+	
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>functions recursive</title>
+	</head>
+	<body>
+		<h1>functions recursive</h1>
+		<h2>deel 1</h2>
+		<p></p>
+		<h2>deel 2</h2>
+		<p></p>
+	</body>
+</html>
