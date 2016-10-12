32413143fdd1fb7f5604d5c5f08332b7c1b3e618
TVke <thomas.verhelst@student.kdg.be>
2016-10-12 23:34:01 +0200

    add start of solution functions advanced

---
+++ oplossing-functions-gevorderd/index.php
@@ -0,0 +1,18 @@
+<?php
+	
+	// deel 1
+	
+	
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>functions gevorderd</title>
+	</head>
+	<body>
+		<h1>functions gevorderd</h1>
+		<h2>deel 1</h2>
+		<p></p>
+	</body>
+</html>
