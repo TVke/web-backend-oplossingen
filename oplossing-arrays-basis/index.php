15c489bfdc5b66640880676a806b03577da6f997
Thomas <Thomas@Pro-van-Thomas.local>
2016-10-12 18:50:12 +0200

    add start file arrays basics solution

---
+++ oplossing-arrays-basis/index.php
@@ -0,0 +1,22 @@
+<?php
+	
+	// deel 1
+	
+	
+	// deel 2
+	
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>arrays basis</title>
+	</head>
+	<body>
+		<h1>arrays basis</h1>
+		<h2>deel 1</h2>
+		<p></p>
+		<h2>deel 2</h2>
+		<p></p>
+	</body>
+</html>
