a9e0ee126fe19c0bc10f6f7d5c7cd3b6117f3eb2
TVke <thomas.verhelst@student.kdg.be>
2016-10-13 09:50:21 +0200

    add start of solution functions recursive

--- oplossing-functions-recursive/index.php
+++ oplossing-functions-recursive/index.php
@@ -1,7 +1,21 @@
 <?php
 	
 	// deel 1
+	$startBedrag=100000;
+	$renteProcent=8;
+	$jaarSparen=10;
 	
+	function plusRenteOpJaarbasis($start,$rente,$jaar){
+		static $jaren=0;
+		if($jaren<$jaar){
+			$start+=$start*$rente/100;
+// 			plusRenteOpJaarbasis($start,$rente,$jaar);
+		}
+		else{
+			return $start;
+		}
+		$jaren++;
+	}
 	
 	// deel 2
 	
@@ -15,7 +29,7 @@
 	<body>
 		<h1>functions recursive</h1>
 		<h2>deel 1</h2>
-		<p></p>
+		<p>Het bedrag €<?= $startBedrag?> met een rente van <?= $renteProcent?>% word na <?= $jaarSparen?> jaar €<?= plusRenteOpJaarbasis($startBedrag,$renteProcent,$jaarSparen)?></p>
 		<h2>deel 2</h2>
 		<p></p>
 	</body>
