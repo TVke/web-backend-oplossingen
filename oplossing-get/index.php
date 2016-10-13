f50702360d5955f209b1a288616b13fbc566d244
TVke <thomas.verhelst@student.kdg.be>
2016-10-13 15:16:02 +0200

    add start of get solution

---
+++ oplossing-get/index.php
@@ -0,0 +1,17 @@
+<?php
+	
+	// deel 1
+	
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>functions recursive</title>
+	</head>
+	<body>
+		<h1>functions recursive</h1>
+		<h2>deel 1</h2>
+		<p></p>
+	</body>
+</html>
