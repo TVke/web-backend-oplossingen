83929b227caab70daf00f73c2051934c174aa77b
TVke <thomas.verhelst@student.kdg.be>
2016-10-18 18:34:56 +0200

    added final solution of post

--- oplossing-post/index.php
+++ oplossing-post/index.php
@@ -1,17 +1,40 @@
 <?php
 	
-	
-	
+	$username='stijn';
+	$password='azerty';
+	$fout=false;
+	$correct=false;
+	$message="Welkom ".$username;
+	$wrongMessage="Er ging iets mis bij het inloggen, probeer opnieuw";
+	if(isset($_POST['send'])){
+		if($_POST['user']==$username && $_POST['pass']=$password){
+			$correct=true;
+		}else{
+			$fout=true;
+		}
+	}
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>POST</title>
-		<link href="stijl.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
 		<h1>POST</h1>
 		<h2>Deel 1</h2>
+		<form name="inloggen" action="index.php" method="post">
+			<label for="username">gebruikersnaam</label>
+			<input type="text" name="user" id="username">
+			<label for="password">paswoord</label>
+			<input type="password" name="pass" id="password">
+			<input type="submit" value="inloggen" name="send">
+		</form>
+		<?php if($correct):?>
+		<p><?= $message?></p>
+		<?php endif;?>
+		<?php if($fout):?>
+		<p><?= $wrongMessage?></p>
+		<?php endif;?>
 	</body>
 </html>
