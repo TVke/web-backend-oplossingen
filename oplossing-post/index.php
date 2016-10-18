0b27eef36435ab5f9df458780a6a30de72d2aee0
TVke <thomas.verhelst@student.kdg.be>
2016-10-18 12:03:53 +0200

    add start of solution post

---
+++ oplossing-post/index.php
@@ -0,0 +1,17 @@
+<?php
+	
+	
+	
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>POST</title>
+		<link href="stijl.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<h1>POST</h1>
+		<h2>Deel 1</h2>
+	</body>
+</html>
