1b490660f90b84666d42b13f38519d3c1b42cea3
TVke <thomas.verhelst@student.kdg.be>
2016-10-20 17:51:36 +0200

    almost finished solution of date task

--- oplossing-date/index.php
+++ oplossing-date/index.php
@@ -1,7 +1,9 @@
 <?php
 
-
-
+	$datum=mktime(22,35,25,1,21,1904);
+	$fullDate= date("d F Y, h:i:s a",$datum);
+setlocale('LC_ALL', 'nl_NL');
+	$dutchDate= strftime("%d %B %Y, %I:%M:%S %P",$datum);
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,6 +13,9 @@
 <body>
 	<h1>date</h1>
 	<h2>deel 1</h2>
+	<p>de timestamp is <?= $datum?></p>
+	<p>de datum is <?= $fullDate?></p>
 	<h2>deel 2</h2>
+	<p>de datum is <?= $fullDate?></p>
 </body>
 </html>
