342b961be5b95fe0484e30a3d39b06b74688b088
TVke <thomas.verhelst@student.kdg.be>
2016-10-27 16:32:06 +0200

    added solution include/require

---
+++ oplossing-include-require/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$artikels = array(
+	array(
+		"titel"=>"PHP 8.4 gereleased",
+		"text"=>"De nieuwste versie van PHP bevat uitbreidingen op oa. mind control-facades, in-browser 3d projecties en de API voor drones.",
+		"tags"=>array("new","PHP 8.4")
+	),
+	array(
+		"titel"=>"Zymphovel framework",
+		"text"=>"Wordt Zymphovel het nieuwste PHP-framework dat de wereld in een sneltempo zal veroveren?",
+		"tags"=>array("frameworks")
+	),
+	array(
+			"titel"=>"Rasmus Lerdorf vermist",
+			"text"=>"De geestelijke vader van PHP Rasmus Lerdorf is sinds vorige week vermist nadat hij met zijn privé-jet richting Ibiza vertrok.",
+			"tags"=>array("bizar","rasmus","lerdorf","feestneus")
+	)
+);
+
+require("view/header.html");
+require("view/body.html");
+require("view/footer.html");
